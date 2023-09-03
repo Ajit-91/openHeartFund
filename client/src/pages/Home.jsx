4abc1548f994411e3e66e119c913a4ff0ef8e7ce
Ajit-91 <ajitkumar12105@gmail.com>
2023-09-03 22:53:39 +0530

    modified home and profile page

--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -12,21 +12,31 @@ const Home = () => {
   const fetchCampaigns = async () => {
     setIsLoading(true);
     const data = await getCampaigns();
-    
+
     setCampaigns(data);
     setIsLoading(false);
   }
 
   useEffect(() => {
-    if(contract) fetchCampaigns();
+    if (contract) fetchCampaigns();
   }, [address, contract]);
 
   return (
-    <DisplayCampaigns 
-      title="All Campaigns"
-      isLoading={isLoading}
-      campaigns={campaigns}
-    />
+    <>
+      <div className="bg-[#1dc071] rounded-xl mb-5 py-12 px-4 text-center text-white">
+        <h1 className="text-4xl font-semibold mb-4">Discover Inspiring Campaigns</h1>
+        <p className="text-lg mb-8">
+          Explore a world of possibilities and make a difference. 
+          Here, you can find a wide range of inspiring campaigns created by users just like you.
+        </p>
+      </div>
+
+      <DisplayCampaigns
+        title="All Campaigns"
+        isLoading={isLoading}
+        campaigns={campaigns}
+      />
+    </>
   )
 }
 
--- client/src/pages/Profile.jsx
+++ client/src/pages/Profile.jsx
@@ -21,11 +21,17 @@ const Profile = () => {
   }, [address, contract]);
 
   return (
-    <DisplayCampaigns
-      title="All Campaigns"
-      isLoading={isLoading}
-      campaigns={campaigns}
-    />
+    <>
+      <div className="bg-[#1dc071] rounded-xl mb-5 py-12 px-4 text-center text-white">
+        <h1 className="text-4xl font-semibold mb-4">Your Campaigns</h1>
+        <p className="text-lg mb-8">Browse and manage the campaigns you've created. Make an impact and track your progress.</p>
+      </div>
+      <DisplayCampaigns
+        title="All Campaigns"
+        isLoading={isLoading}
+        campaigns={campaigns}
+      />
+    </>
   )
 }
 
